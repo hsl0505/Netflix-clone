ca6730896217d14ce8ee3ab436dac6c93fcfda05
hyunsung <hslgood@gmail.com>
2020-03-20 07:24:01 +0900

    Add InitPage

---
+++ src/pages/InitPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import InitPageHeader from '../components/InitPage/InitPageHeader';
+
+export default function InitPage(): JSX.Element {
+  return (
+    <div>
+      <InitPageHeader />
+    </div>
+  );
+}
