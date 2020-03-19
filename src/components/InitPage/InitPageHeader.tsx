750deb51b7ce227b888ae8f2592c0d75845ec68e
hyunsung <hslgood@gmail.com>
2020-03-20 07:23:42 +0900

    Add InitPageHeader component

---
+++ src/components/InitPage/InitPageHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function InitPageHeader(): JSX.Element {
+  return (
+    <div className="InitPageHeader">
+      <div className="title">NETFLIX</div>
+      <div className="login">로그인</div>
+    </div>
+  );
+}
