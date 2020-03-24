9c75dc6d428b92f19dfd8699f35ec7abc665124e
hyunsung <hslgood@gmail.com>
2020-03-24 22:57:03 +0900

    Add main text at initpage header

--- src/components/InitPage/InitPageHeader.tsx
+++ src/components/InitPage/InitPageHeader.tsx
@@ -3,8 +3,18 @@ import React from 'react';
 export default function InitPageHeader(): JSX.Element {
   return (
     <div className="InitPageHeader">
-      <div className="title">NETFLIX</div>
-      <div className="login">로그인</div>
+      <div className="backImg">
+        <div className="logoLogin">
+          <div className="logo">NETFLIX</div>
+          <div className="login">로그인</div>
+        </div>
+
+        <div className="mainTextBox">
+          <div className="text1">영화, TV 프로그램을</div>
+          <div className="text2">무제한으로.</div>
+          <div className="text3">다양한 디바이스에서 시청하세요. 언제든 해지하실 수 있습니다.</div>
+        </div>
+      </div>
     </div>
   );
 }
