6d31c6c8929f8a723454f73d23b1ec50d93bf7ac
hyunsung <hslgood@gmail.com>
2020-03-26 04:55:17 +0900

    Add InitPageContent component at InitPage

--- src/pages/InitPage.tsx
+++ src/pages/InitPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import InitPageHeader from '../components/InitPage/InitPageHeader';
+import InitPageContent from '../components/InitPage/InitPageContent';
 
 export default function InitPage(): JSX.Element {
   return (
     <div className="InitPage">
       <InitPageHeader />
+      <InitPageContent />
     </div>
   );
 }
