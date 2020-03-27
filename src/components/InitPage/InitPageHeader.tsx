bd85bfc72f0b9ddc1cdfed298a66e73a97ba1a0d
hyunsung <hslgood@gmail.com>
2020-03-27 19:52:18 +0900

    Add text at initPageHeader

--- src/components/InitPage/InitPageHeader.tsx
+++ src/components/InitPage/InitPageHeader.tsx
@@ -16,6 +16,9 @@ export default function InitPageHeader(): JSX.Element {
           <div className="text2">무제한으로.</div>
           <div className="text3">다양한 디바이스에서 시청하세요. 언제든 해지하실 수 있습니다.</div>
           <InitPageEmailInput />
+          <div className="text4">
+            시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일 주소를 입력하세요.
+          </div>
         </div>
       </div>
     </div>
