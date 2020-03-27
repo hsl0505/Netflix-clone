2ee9796f51108b18fe6a2863d402d215bb3351b6
hyunsung <hslgood@gmail.com>
2020-03-27 19:53:43 +0900

    Add initPageQnA component at initPage

--- src/pages/InitPage.tsx
+++ src/pages/InitPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import InitPageHeader from '../components/InitPage/InitPageHeader';
 import InitPageContent from '../components/InitPage/InitPageContent';
+import InitPageQnA from '../components/InitPage/InitPageQnA';
 
 export default function InitPage(): JSX.Element {
   return (
     <div className="InitPage">
       <InitPageHeader />
       <InitPageContent />
+      <InitPageQnA />
     </div>
   );
 }
