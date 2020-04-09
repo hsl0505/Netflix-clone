55aab72d38c258a315bb097bbae25f52ef7caa35
hyunsung <hslgood@gmail.com>
2020-04-09 13:50:03 +0900

    Add redux store and redux devtool

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './scss/index.scss';
+
 import { BrowserRouter } from 'react-router-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/modules';
+
 import App from './App';
 
+const devTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
+
+const store = createStore(rootReducer, devTools && devTools());
+// console.log(store.getState());
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
