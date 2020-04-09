cf8d99ee682b9fa7b3f9ea9f762ec53c061da2b1
hyunsung <hslgood@gmail.com>
2020-04-09 13:49:37 +0900

    Add login page (route)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-// import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import InitPage from './pages/InitPage';
+import LoginPage from './pages/LoginPage';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={InitPage} />
+        <Route exact path="/" component={InitPage} />
+        <Route path="/login" component={LoginPage} />
       </Switch>
     </div>
   );
