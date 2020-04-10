035d260b00133478b664c8b511f4d5a042bce989
hyunsung <hslgood@gmail.com>
2020-04-10 23:14:39 +0900

    Add showQnAAns reducer

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import emailCheck, { EmailState } from './emailCheck';
+import showQnAAns, { ShowAnsState } from './showQnA_Ans';
 
 export interface State {
   emailCheck: EmailState;
+  showQnAAns: ShowAnsState;
 }
 
-export default combineReducers({ emailCheck });
+export default combineReducers({ emailCheck, showQnAAns });
