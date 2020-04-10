43f52990ef28b8c32d1f180eda356abb4a4d77d3
hyunsung <hslgood@gmail.com>
2020-04-10 23:12:16 +0900

    Add initPageQnA box components

---
+++ src/components/initPage/InitPageQnABox.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import InitPageQnABoxAns from './InitPageQnABoxAns';
+
+interface Props {
+  handleClick: Function;
+  showArr: number[];
+  contents: {
+    id: number;
+    title: string;
+    content: string;
+  };
+}
+
+export default function InitPageQnABox(props: Props): JSX.Element {
+  const { contents, handleClick, showArr } = props;
+  const { id, title, content } = contents;
+  return (
+    <div className="InitPageQnABox">
+      <div className="QnA_Box_Title" onClick={(): void => handleClick(id)}>
+        <div className="title_text">{title}</div>
+        <div className="title_plus">{showArr[id] === 1 ? '<' : '+'}</div>
+      </div>
+      {showArr[id] === 1 ? <InitPageQnABoxAns content={content} /> : <div />}
+    </div>
+  );
+}
