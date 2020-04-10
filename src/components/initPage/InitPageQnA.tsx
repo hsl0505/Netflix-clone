73cbd9bd5451529fd29983da14311ca841e81400
hyunsung <hslgood@gmail.com>
2020-04-10 23:11:51 +0900

    Add initPageQnA (title, box container, emailInput)

--- src/components/initPage/InitPageQnA.tsx
+++ src/components/initPage/InitPageQnA.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import InitPageQnABoxContainer from '../../containers/InitPageQnABoxContainer';
+import InitPageEmailInputContainer from '../../containers/InitPageEmailInputContainer';
 
 export default function InitPageQnA(): JSX.Element {
-  return <div className="InitPageQnA">큐앤에이</div>;
+  return (
+    <div className="InitPageQnA">
+      <div className="QnA_Title">자주 묻는 질문</div>
+      <InitPageQnABoxContainer />
+      <InitPageEmailInputContainer />
+      <div className="QnA_emailText">
+        시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일 주소를 입력하세요.
+      </div>
+    </div>
+  );
 }
