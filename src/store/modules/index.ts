68e296936f2abdbfeb8ee350f1e3942352b4a138
hyunsung <hslgood@gmail.com>
2020-04-13 03:19:11 +0900

    Add select language reducer

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import emailCheck, { EmailState } from './emailCheck';
 import showQnAAns, { ShowAnsState } from './showQnA_Ans';
+import selectLanguage, { LanguageState } from './selectLanguage';
 
 export interface State {
   emailCheck: EmailState;
   showQnAAns: ShowAnsState;
+  selectLanguage: LanguageState;
 }
 
-export default combineReducers({ emailCheck, showQnAAns });
+export default combineReducers({ emailCheck, showQnAAns, selectLanguage });
