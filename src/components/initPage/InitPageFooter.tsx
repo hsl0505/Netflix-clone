c404dc5b416d572f202dc2990bab5ed0d72ecd3f
hyunsung <hslgood@gmail.com>
2020-04-22 08:39:59 +0900

    Add footer country and info

--- src/components/initPage/InitPageFooter.tsx
+++ src/components/initPage/InitPageFooter.tsx
@@ -34,6 +34,20 @@ export default function InitPageFooter(): JSX.Element {
           </div>
         </div>
         <InitPageFooterSelectContainer />
+        <div className="footer_country">Netflix 대한민국</div>
+        <div className="footer_info">
+          넷플릭스서비시스코리아 유한회사 통신판매업신고번호: 제2018-서울종로-0426호 전화번호:
+          <br />
+          대표: 레지널드 숀 톰프슨
+          <br />
+          이메일 주소: korea@netflix.com
+          <br />
+          주소: 대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층 우편번호 03161
+          <br />
+          클라우드 호스팅: Amazon Web Services Inc.
+          <br />
+          공정거래위원회 웹사이트 링크
+        </div>
       </div>
     </div>
   );
