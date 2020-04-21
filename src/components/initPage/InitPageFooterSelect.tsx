155b277a926376f0e8be73ff1f3f4f6a97345b66
hyunsung <hslgood@gmail.com>
2020-04-22 08:40:51 +0900

    Fix select box (from material ui to html )

--- src/components/initPage/InitPageFooterSelect.tsx
+++ src/components/initPage/InitPageFooterSelect.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormControl, Select, MenuItem, ListItemIcon, ListItemText } from '@material-ui/core';
+
 import LanguageIcon from '@material-ui/icons/Language';
 
 interface Props {
@@ -11,20 +11,16 @@ export default function InitPageFooterSelect(props: Props): JSX.Element {
   const { handleChange, language } = props;
   return (
     <div className="InitPageFooterSelect">
-      <FormControl className="temp" variant="outlined">
-        <Select displayEmpty value="" onChange={(e): void => handleChange(e.target.value)}>
-          <MenuItem value="" disabled className="temp3">
-            <div className="temp2">
-              <ListItemIcon>
-                <LanguageIcon />
-              </ListItemIcon>
-              <ListItemText primary={language} />
-            </div>
-          </MenuItem>
-          <MenuItem value="한국어">한국어</MenuItem>
-          <MenuItem value="English">English</MenuItem>
-        </Select>
-      </FormControl>
+      <LanguageIcon className="select_icon" />
+      <select
+        className="select_box"
+        value={language}
+        onChange={(e): void => handleChange(e.target.value)}
+      >
+        <option value="한국어">한국어</option>
+        <option value="English">English</option>
+      </select>
+      <div className="select_arrow" />
     </div>
   );
 }
